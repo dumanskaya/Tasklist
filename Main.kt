0f025dea0e8a5b4229dd94986167a033bb57a40e
Maria Dumanskaya <masha.dumanskaya@gmail.com>
2023-01-03 19:25:41 +0100

    implemented task date

--- Main.kt
+++ Main.kt
@@ -1,11 +1,62 @@
 package tasklist
+import kotlinx.datetime.*
+
+interface Parameter {
+    fun isValid(value: String): Boolean
+}
+
+class TaskDate: Parameter {
+    var date: String = ""
+        set(value) {
+            if(isValid(value)) {
+                field = value
+            }
+        }
+
+    override fun isValid(value: String): Boolean {
+        val parts = value.split('-')
+        if (parts.size != 3) return false
+        try {
+            parts.map { it.toInt() }
+        } catch (e: NumberFormatException) {
+            return false
+        }
+        try {
+            val (year, month, day) = parts.map { it.toInt() }
+            LocalDate(year, month, day)
+        } catch (e: IllegalArgumentException){
+            return false
+        }
+        return true
+    }
+
+    fun setValueFromUser(){
+        println("Input the date (yyyy-mm-dd):")
+        var line = readln()
+        while (!isValid(line)) {
+            println("The input date is invalid")
+            println("Input the date (yyyy-mm-dd):")
+            line = readln()
+        }
+        this.date = line
+    }
+
+    override fun toString(): String {
+        return this.date
+    }
+}
+
+
 
 class Task {
     var lines = mutableListOf<String>()
-
+    var date: TaskDate = TaskDate()
 }
+
 fun createTask(): Task? {
     val task = Task()
+    task.date.setValueFromUser()
+
     println("Input a new task (enter a blank line to end):")
     var line = readln().trim()
     if (line.isEmpty()) {
@@ -24,9 +75,10 @@ fun printTasks(tasks: MutableList<Task>) {
         println("No tasks have been input")
     } else {
         for (i in tasks.indices) {
+            val sep = (i + 1).toString().padEnd(3)
+            println("$sep${tasks[i].date}")
             for (j in tasks[i].lines.indices) {
-                val sep = if (j == 0) (i + 1).toString().padEnd(3) else "   "
-                println("$sep${tasks[i].lines[j]}")
+                println("   ${tasks[i].lines[j]}")
             }
             println()
         }
